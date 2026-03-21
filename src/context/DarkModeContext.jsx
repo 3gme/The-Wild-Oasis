d7274851b35328f2501ef373761cbb10834b3a61
mahmoud ezzat <mahmoud.ezzat1234123121@gmail.com>
2026-03-21 22:53:57 +0200

    dark mode feature added

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode],
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider
+      value={{ isDarkMode, setIsDarkMode, toggleDarkMode }}
+    >
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkModeContext() {
+  const context = useContext(DarkModeContext);
+  if (!context) throw new Error("DarkModeContext used outside it's scope");
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkModeContext };
